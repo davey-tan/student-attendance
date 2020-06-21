b6bff04e4bdedf3cc095e7c37434b66d8963a80b
Davey <davey_tan@tutanota.com>
2020-06-21 16:01:02 +0700

    Added example line

--- src/student/attendance/MainClass.java
+++ src/student/attendance/MainClass.java
@@ -16,6 +16,8 @@ public class MainClass {
      */
     public static void main(String[] args) {
         System.out.println("Why are you gay.");
+        
+        System.out.println("NOO YOU ARE GAYYYYYY");
     }
     
 }
